2661b24ca0b8f3aa3456026241cb86c0cd857467
Magnus Øyhovden <magnufoy@Magnuss-MacBook-Air.local>
2023-03-01 22:11:11 +0100

    Added main methods in State and GameStateManager

--- core/src/com/mygdx/game/View/GameStateManager.java
+++ core/src/com/mygdx/game/View/GameStateManager.java
@@ -1,4 +1,28 @@
 package com.mygdx.game.View;
 
+import java.util.Stack;
+
 public class GameStateManager {
+    private Stack<State> states;
+    
+    public GameStateManager(){
+        states = new Stack<State>();
+    }
+
+    public void push(State state){
+        states.push(state);
+    }
+
+    public void set(State state){
+        states.pop().dispose();
+        states.push(state);
+    }
+
+    public void update(float dt){
+        states.peek().update(dt);
+    }
+
+    public State getCurrentState(){
+        return states.peek();
+    }
 }
--- core/src/com/mygdx/game/View/State.java
+++ core/src/com/mygdx/game/View/State.java
@@ -1,4 +1,16 @@
 package com.mygdx.game.View;
 
-public class State {
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public abstract class State {
+
+    protected GameStateManager gsm;
+
+    protected State(GameStateManager gsm){
+        this.gsm = gsm;
+    }
+
+    public abstract void update(float dt);
+    public abstract void render(SpriteBatch sb);
+    public abstract void dispose();
 }
