501b75246e09cf95eae4bdb1094d99af93c61a30
ChristianKartveit <chrigk@stud.ntnu.no>
2023-03-15 10:08:52 +0100

    Fixed project

--- android/src/com/mygdx/game/AndroidLauncher.java
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.game.Controller.GameController;
+import com.mygdx.game.GameController;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
