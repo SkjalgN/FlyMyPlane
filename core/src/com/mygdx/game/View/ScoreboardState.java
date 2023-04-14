a46b0c15e67db44ac54e2f99f32aae0d03ec2b3f
ChristianKartveit <chrigk@stud.ntnu.no>
2023-04-14 10:39:04 +0200

    added backbutton to scoreboard

--- core/src/com/mygdx/game/View/ScoreboardState.java
+++ core/src/com/mygdx/game/View/ScoreboardState.java
@@ -6,6 +6,10 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.mygdx.game.API;
 import com.mygdx.game.Model.Score;
 
@@ -15,12 +19,41 @@ public class ScoreboardState extends State{
     private API database;
     private ArrayList<Score> scoreboardList;
     private Texture background;
-    protected ScoreboardState(GameStateManager gsm, API Database) {
+    private Skin backSkin;
+    private Button backButton;
+    private GameStage stage;
+    protected ScoreboardState(final GameStateManager gsm, API Database) {
         super(gsm);
         this.database = Database;
         background = new Texture("gamescreens/scoreboard.jpg");
         scoreboardList = new ArrayList<>();
         database.getHighscores(scoreboardList);
+
+        // Create a stage
+        stage = new GameStage();
+
+        //Load a skin from a JSNO file
+        backSkin = new Skin(Gdx.files.internal("buttons/game/leftBtn/leftBtn.json"));
+
+        //Create a button
+        backButton = new Button(backSkin);
+
+        //Set button position, size and function
+        backButton.setPosition(0, 0);
+        backButton.setSize(200, 200);
+        backButton.addListener(new InputListener() {
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                gsm.set(new MenuState(gsm, database));
+                System.out.println("Button Pressed");
+                return true;
+            }
+
+
+        });
+
+    stage.addActor(backButton);
+    Gdx.input.setInputProcessor(stage);
     }
 
     @Override
@@ -52,11 +85,14 @@ public class ScoreboardState extends State{
             }
         }
         sb.end();
+        stage.act(Gdx.graphics.getDeltaTime());
+        stage.draw();
         font.dispose();
     }
 
     @Override
     public void dispose() {
-
+        stage.dispose();
+        backSkin.dispose();
     }
 }
