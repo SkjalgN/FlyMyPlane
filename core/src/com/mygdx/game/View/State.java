19ca64de993d9c51867129d359ded3a7ad3b9234
ChristianKartveit <chrigk@stud.ntnu.no>
2023-04-14 11:38:15 +0200

    added global heigh and width attribute

--- core/src/com/mygdx/game/View/State.java
+++ core/src/com/mygdx/game/View/State.java
@@ -1,10 +1,12 @@
 package com.mygdx.game.View;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public abstract class State {
-
+    protected int width = Gdx.graphics.getWidth();
+    protected int height = Gdx.graphics.getHeight();
     protected GameStateManager gsm;
     protected OrthographicCamera cam;
 
