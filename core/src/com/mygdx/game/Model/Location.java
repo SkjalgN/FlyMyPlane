897b2fb1fbfdc9c794e29825c8d92731333d027e
Andreas Maakestad <andremaa@stud.ntnu.no>
2023-04-15 15:24:07 +0200

    Added location class

---
+++ core/src/com/mygdx/game/Model/Location.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.Model;
+
+public class Location {
+
+    private String locationName;
+    private float xPos;
+    private float yPos;
+
+    //Constructor
+    public Location(String locationName, float xPos, float yPos) {
+        this.locationName = locationName;
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+}
