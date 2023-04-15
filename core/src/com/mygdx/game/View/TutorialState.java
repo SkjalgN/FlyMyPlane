aa070e20758aa879ba7270b813943dc7f0a6e0c1
Skjalgn <skjalgn@stud.ntnu.no>
2023-04-15 16:26:05 +0200

    lagde litt tutorialstate

--- core/src/com/mygdx/game/View/TutorialState.java
+++ core/src/com/mygdx/game/View/TutorialState.java
@@ -8,26 +8,55 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.mygdx.game.API;
+
 
 public class TutorialState extends State{
     private SpriteBatch batch;
-    private BitmapFont font = new BitmapFont();
     private Texture background;
+    private Button nextBtn;
+    private Skin nextBtnSkin;
+    private GameStage stage;
+    private API database;
     TextButton.TextButtonStyle textButtonStyle;
-
-    private float WIDTH = Gdx.graphics.getWidth();
-    private float HEIGHT = Gdx.graphics.getHeight();
     
 
     public TutorialState(GameStateManager gsm) {
         super(gsm);
         batch = new SpriteBatch();
-        System.out.println(WIDTH);
-        System.out.println(HEIGHT);
-        cam.setToOrtho(false, WIDTH * 4, HEIGHT * 4);
+        background = new Texture("gamescreens/tutorial1.jpg");
+
+                // Create a stage
+                stage = new GameStage();
+
+                // Load a skin from JSON file
+                nextBtnSkin = new Skin(Gdx.files.internal("buttons/game/rightBtn/rightBtn.json"));
         
-        background = new Texture("gamescreens/mapClean.jpg");
-    }
+                // Create a button
+                nextBtn = new Button(nextBtnSkin);
+        
+                // Set button position, size and function
+                nextBtn.setSize(width / 8f, width / 8f);
+                nextBtn.setPosition(width - nextBtn.getWidth() * 1.2f, height / 2f - (nextBtn.getHeight() / 2f));
+                nextBtn.addListener(new InputListener() {
+                    @Override
+                    public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                        gsm.set(new MenuState(gsm, database));
+                        System.out.println("Button Pressed");
+                        return true;
+                    }
+        
+                });
+        
+                stage.addActor(nextBtn);
+                Gdx.input.setInputProcessor(stage);
+            }
+    
 
     @Override
     public void update(float dt) {
@@ -36,17 +65,9 @@ public class TutorialState extends State{
 
     @Override
     public void render(SpriteBatch sb) {
-        sb.setProjectionMatrix(cam.combined);
+
         sb.begin();
-        sb.draw(background, WIDTH/4, HEIGHT/4);
-        textButtonStyle = new TextButtonStyle();
-        textButtonStyle.font = font;
-        textButtonStyle.fontColor = Color.WHITE;
-
-
-        TextButton textButton = new TextButton("Sjekk her da", textButtonStyle);
-        textButton.setPosition(WIDTH, WIDTH);
-        textButton.draw(sb, 1);
+        sb.draw(background, 0,0,width,height);
         sb.end();
 
     }
