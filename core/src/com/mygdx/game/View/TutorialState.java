774257cbe891f5cbe178ae8e710e5191591ee001
Magnus Øyhovden <magnufoy@gmail.com>
2023-04-17 15:13:34 +0200

    toturial

--- core/src/com/mygdx/game/View/TutorialState.java
+++ core/src/com/mygdx/game/View/TutorialState.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
@@ -13,9 +14,14 @@ import com.mygdx.game.API;
 
 public class TutorialState extends State{
     private SpriteBatch batch;
-    private Texture background;
+    private TextureRegion background[];
+    private int backgroundVar = 0;
     private Button nextBtn;
+    private Button exitBtn;
+    private Button backBtn;
     private Skin nextBtnSkin;
+    private Skin exitBtnSkin;
+    private Skin backBtnSkin;
     private GameStage stage;
     private API database;
     TextButton.TextButtonStyle textButtonStyle;
@@ -24,21 +30,38 @@ public class TutorialState extends State{
     public TutorialState(final GameStateManager gsm, final API database) {
         super(gsm);
         batch = new SpriteBatch();
-        background = new Texture("gamescreens/tutorial1.jpg");
-
+        background = new TextureRegion[2];
+        background[0] = new TextureRegion(new Texture("gamescreens/tutorial1.jpg"));
+        background[1] = new TextureRegion(new Texture("gamescreens/tutorial2.jpg"));
+        backgroundVar = 0;
                 // Create a stage
                 stage = new GameStage();
 
                 // Load a skin from JSON file
-                nextBtnSkin = new Skin(Gdx.files.internal("buttons/game/rightBtn/rightBtn.json"));
+                nextBtnSkin = new Skin(Gdx.files.internal("buttons/tutorial/next/next.json"));
+                exitBtnSkin = new Skin(Gdx.files.internal("buttons/tutorial/exit/exit.json"));
+                backBtnSkin = new Skin(Gdx.files.internal("buttons/tutorial/back/back.json"));
         
                 // Create a button
                 nextBtn = new Button(nextBtnSkin);
-        
+                exitBtn = new Button(exitBtnSkin);
+                backBtn = new Button(backBtnSkin);
+
                 // Set button position, size and function
-                nextBtn.setSize(width / 8f, width / 8f);
-                nextBtn.setPosition(width - nextBtn.getWidth() * 1.2f, height / 2f - (nextBtn.getHeight() / 2f));
+                nextBtn.setSize(width / 7f, height / 11f);
+                nextBtn.setPosition(width*5f/8f, height*1f/8f);
                 nextBtn.addListener(new InputListener() {
+                    @Override
+                    public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                        System.out.println("Button Pressed");
+                        backgroundVar = 1;
+                        return true;
+                    }
+                });
+
+                exitBtn.setSize(width / 7f, height / 11f);
+                exitBtn.setPosition(width*3f/8f, height*1f/8f);
+                exitBtn.addListener(new InputListener() {
                     @Override
                     public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
                         gsm.set(new MenuState(gsm, database));
@@ -47,8 +70,21 @@ public class TutorialState extends State{
                     }
         
                 });
+
+                backBtn.setSize(width / 7f, height / 11f);
+                backBtn.setPosition(width*1f/8f, height*1f/8f);
+                backBtn.addListener(new InputListener() {
+                    @Override
+                    public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                        backgroundVar = 0;
+                        return true;
+                    }
+        
+                });
         
                 stage.addActor(nextBtn);
+                stage.addActor(exitBtn);
+                stage.addActor(backBtn);
                 Gdx.input.setInputProcessor(stage);
             }
     
@@ -62,7 +98,7 @@ public class TutorialState extends State{
     public void render(SpriteBatch sb) {
 
         sb.begin();
-        sb.draw(background, 0,0,width,height);
+        sb.draw(background[backgroundVar], 0,0,width,height);
         sb.end();
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
@@ -70,8 +106,5 @@ public class TutorialState extends State{
 
     @Override
     public void dispose() {
-
-        background.dispose();
-
     }
 }
