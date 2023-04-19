052a7c2bcc6f538ac07312ef45d8c2378eb2d724
Magnus Øyhovden <magnufoy@gmail.com>
2023-04-19 17:41:43 +0200

    clean controller

--- core/src/com/mygdx/game/Controller/GameController.java
+++ core/src/com/mygdx/game/Controller/GameController.java
@@ -11,16 +11,16 @@ import com.mygdx.game.View.GameState;
 import com.mygdx.game.View.MenuState;
 import com.mygdx.game.View.GameStateManager;
 import com.mygdx.game.View.StartGameState;
-import com.mygdx.game.View.TestScreen;
+import com.mygdx.game.View.TutorialScreen;
 import com.mygdx.game.View.VictoryState;
 
 import java.util.ArrayList;
 
 
 public class GameController extends Game {
-	private GameStateManager gsm;
-	private  SpriteBatch batch;
-	private TestScreen testScreen;
+	//private GameStateManager gsm;
+	//private  SpriteBatch batch;
+	private TutorialScreen tutorialScreen;
 
 	private API _FBIC;
 
@@ -30,10 +30,10 @@ public class GameController extends Game {
 	public ArrayList<Score> listen;
 	@Override
 	public void create() {
-		batch = new SpriteBatch();
-		testScreen = new TestScreen(this);
-		setScreen(testScreen);
-		testScreen.show();
+		//batch = new SpriteBatch();
+		tutorialScreen = new TutorialScreen(this);
+		setScreen(tutorialScreen);
+		tutorialScreen.show();
  		//gsm = new GameStateManager();
 		//gsm.push(new StartGameState(gsm, _FBIC));
 		//listen = new ArrayList<>();
@@ -52,12 +52,6 @@ public class GameController extends Game {
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		//gsm.update(Gdx.graphics.getDeltaTime());
 		//gsm.render(batch);
-		testScreen.render(Gdx.graphics.getDeltaTime());
-
-	}
-
-	public void handleInput() {
-		
-		
+		tutorialScreen.render(Gdx.graphics.getDeltaTime());
 	}
 }
